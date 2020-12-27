f963b2e5558f43cce1e5dfefe49b7dc5735f4369
Anatis <broberg.daniel89@gmail.com>
2020-12-27 14:04:35 +0100

    update

--- frontend/f2p-gamesite/src/Pages/GameImport/Gameimport.js
+++ frontend/f2p-gamesite/src/Pages/GameImport/Gameimport.js
@@ -5,16 +5,30 @@ import GameTemplate from "./GameTemplate.js"
 
 
 export default class Gameimport extends Component {
+    constructor() {
+        super();
+        this.state = {
+          seminar: [],
+        };
+      }
+      
+      componentDidMount() {
+        fetch('https://localhost:44350/api/seminars')
+          .then((response) => response.json())
+          .then((json) => this.setState({ seminar: json }));
+      }
+
+
+
     render() {
         return (
-                <>
-                  <div class="container">
-                      <div class="row text-center">
-                             <GameTemplate/>
-                             <GameTemplate/>
-                  </div> 
-                  </div> 
-                </>
+            <>
+            <div class="container">
+            <div class="row text-center">
+                    <GameTemplate id={item.id} title={item.title} date={item.date} description={item.description} duration={item.duration}/>
+            </div> 
+            </div> 
+            </>
         )
     }
 }
