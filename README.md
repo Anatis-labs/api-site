81e51b2d2b4dc2c1eea86f745adc7613e07383c1
Anatis <broberg.daniel89@gmail.com>
2021-01-28 15:36:12 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # api-site
 Front end and backend for a api website
+
+Front end application that uses a public api thru the backend .net core
+( https://github.com/Anatis-labs/backend-api-site )
+
+
+The Frontend is build on react, so you will need to have installed in order to npm to run it.
+(install can be found here: https://www.npmjs.com/get-npm)
+
+easiest way to open it is with visual stuido code, in there you can open the terminal and write
+ - npm start
+
+ if this doesnt work you might need to run
+
+ -npm install
+
+ in the terminal before you can start it.
+
+If its booted correctly then it will open in the webreader of your settings choise.
+
+once this is setup you will need to run the corresponding backend ( https://github.com/Anatis-labs/backend-api-site ).
+
+if your unlucky then the the local host adress might differ on another pc making it so that the fetch call doesnt work, and then you need to change it so they match the backend.
+
+they are located on 3 pages in the react files, 
+    # pages/Gameinfo/gameinfo.js
+    # pages/Gameimport/gameimporttype.js
+    # pages/Gameimport/gameimport.js
+
+( looks like this: https://localhost:44392/api/gameinfo/)
--- frontend/f2p-gamesite/src/Pages/GameInfo/GameInfo.js
+++ frontend/f2p-gamesite/src/Pages/GameInfo/GameInfo.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import  { Redirect } from 'react-router-dom'
+
 
 import GameInfoTemplate from "./GameInfoTemplate.js"
 
@@ -13,7 +15,9 @@ export default class GameInfo extends Component {
         };
       }
     
-      componentDidMount() {  
+      componentDidMount() {   
+        if(!Number.isFinite(this.props.match.params.GameId))   
+        {       
         fetch(' https://localhost:44392/api/gameinfo/'+this.props.match.params.GameId)
           .then(result => result.json())
           .then((data) => {
@@ -29,12 +33,20 @@ export default class GameInfo extends Component {
               isLoaded: true,
               error: error
             })
-          );
+          );    
+        }
+        else    
+        {
+          return <Redirect to='/404'  />
+        } 
       }
     
       render() {
         console.log(this.props);
         const { error, isLoaded, items } = this.state;
+        
+        
+        
         if (error) {
           return <div class="container"><h1>Error: {error.message}</h1></div>;
         } else if (!isLoaded) {
@@ -43,7 +55,9 @@ export default class GameInfo extends Component {
           return (
             <>          
             <div class="container">
-            <div class="row text-center">                     
+
+            <div class="row text-center"> 
+                                
               {this.state.items.map((item) => (
                     <GameInfoTemplate
                       id={item.id}
@@ -60,21 +74,18 @@ export default class GameInfo extends Component {
                       release_date={item.release_date}
                       freetogame_profile_url={item.freetogame_profile_url}     
 
-                      
-
                       minimum_system_requirements_os={item.minimum_system_requirements.os}
                       minimum_system_requirements_prossessor={item.minimum_system_requirements.processor}
                       minimum_system_requirements_memory={item.minimum_system_requirements.memory}
                       minimum_system_requirements_graphics={item.minimum_system_requirements.graphics}
-                      minimum_system_requirements_storage={item.minimum_system_requirements.storage}
-
-                      
+                      minimum_system_requirements_storage={item.minimum_system_requirements.storage}                    
                     />
                   ))}                  
                 </div>
               </div>
             </>
           )
-        }
+        }       
       }
     }
+  
